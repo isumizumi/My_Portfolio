79d02befd6b0e6d26cbd585d9b4816a890d1cb48
isumizumi <isumi.karina72@gmail.com>
2017-03-19 01:32:39 +0700

    Release 1: JS & CSS (MVC)

---
+++ 2-JS_Refactor/js/app.js
@@ -0,0 +1,27 @@
+//Source code from repo:
+//
+// $(document).ready(function() {
+//   $('#roller button.add').on('click', function() {
+//     console.log("WAT")
+//     $('.dice').append('<div class="die">0</div>')
+//   })
+//
+//   $('#roller button.roll').on('click', function() {
+//     $('.die').each(function(k, die) {
+//       var value = Math.floor((Math.random()*6)+1)
+//       $(die).text(value)
+//     })
+//   })
+// })
+
+//REFACTOR:
+addDice() => {
+  $('.dice').append('<div class="die">0</div>')
+}
+
+rollDice() => {
+  $('.die').each( (k, die) => {
+    let value = Math.floor((Math.random() * 6) + 1)
+    $('.die').text(value)
+  })
+}
